b3d110ec9e5786e20dd60b2920d8ebf6a5c44864
sundarjikp <sundarjikpeee@gmail.com>
2024-06-13 23:40:10 +0530

    work

---
+++ src/main/java/io/restassured/specification/RequestSpecification.java
@@ -0,0 +1,5 @@
+package io.restassured.specification;
+
+public class RequestSpecification {
+
+}
---
+++ src/main/java/members/BaseClass.java
@@ -0,0 +1,11 @@
+package members;
+
+import io.restassured.response.Response;
+import io.restassured.specification.RequestSpecification;
+
+public class BaseClass {
+	
+	RequestSpecification reqSpec;
+	Response response;
+
+}
